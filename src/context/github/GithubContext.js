303063881367907fd1122782f93c3c6bdb3b97e7
Thyriion <kevinschueler2000@gmail.com>
2022-11-20 14:12:23 +0100

    Refac Userresult

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,12 +8,13 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const res = await fetch(`${GITHUB_URL}/users`);
 
     const data = await res.json();
@@ -23,6 +24,8 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
