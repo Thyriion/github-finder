486c8053f4157f2282f31042a9f2f966846db1de
Thyriion <kevinschueler2000@gmail.com>
2022-11-20 14:27:22 +0100

    Suche der User

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,15 +13,20 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading();
-    const res = await fetch(`${GITHUB_URL}/users`);
 
-    const data = await res.json();
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await res.json();
 
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
@@ -32,7 +37,7 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
       }}
     >
       {children}
