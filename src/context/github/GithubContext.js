97f08f6a20d5348ade03cbf7dc925cdbdb274fa7
Thyriion <kevinschueler2000@gmail.com>
2022-11-24 19:30:42 +0100

    Userseite

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,51 +1,70 @@
-import { createContext, useReducer } from "react";
-import githubReducer from "./GithubReducer";
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const initialState = {
-    users: [],
-    loading: false,
-  };
+    const initialState = {
+        users: [],
+        user: {},
+        loading: false,
+    };
 
-  const [state, dispatch] = useReducer(githubReducer, initialState);
+    const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const searchUsers = async (text) => {
-    setLoading();
+    const searchUsers = async (text) => {
+        setLoading();
 
-    const params = new URLSearchParams({
-      q: text,
-    });
+        const params = new URLSearchParams({
+            q: text,
+        });
 
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`);
+        const res = await fetch(`${GITHUB_URL}/search/users?${params}`);
 
-    const { items } = await res.json();
+        const { items } = await res.json();
 
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
+        dispatch({
+            type: 'GET_USERS',
+            payload: items,
+        });
+    };
 
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+    const getUser = async (login) => {
+        setLoading();
 
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
+        const res = await fetch(`${GITHUB_URL}/users/${login}`);
 
-  return (
-    <GithubContext.Provider
-      value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-      }}
-    >
-      {children}
-    </GithubContext.Provider>
-  );
+        if (res.status === 404) {
+            window.location = '/notfound';
+        } else {
+            const data = await res.json();
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            });
+        }
+    };
+
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
+    const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+    return (
+        <GithubContext.Provider
+            value={{
+                users: state.users,
+                loading: state.loading,
+                user: state.user,
+                searchUsers,
+                clearUsers,
+                getUser,
+            }}>
+            {children}
+        </GithubContext.Provider>
+    );
 };
 
 export default GithubContext;
