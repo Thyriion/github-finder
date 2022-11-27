bf4fc350ee611f1fcb6d3ecc06f0825a44fee1a8
Thyriion <kevinschueler2000@gmail.com>
2022-11-27 20:54:30 +0100

    Refac

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,8 +3,6 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
@@ -15,73 +13,11 @@ export const GithubProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    const searchUsers = async (text) => {
-        setLoading();
-
-        const params = new URLSearchParams({
-            q: text,
-        });
-
-        const res = await fetch(`${GITHUB_URL}/search/users?${params}`);
-
-        const { items } = await res.json();
-
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        });
-    };
-
-    const getUser = async (login) => {
-        setLoading();
-
-        const res = await fetch(`${GITHUB_URL}/users/${login}`);
-
-        if (res.status === 404) {
-            window.location = '/notfound';
-        } else {
-            const data = await res.json();
-
-            dispatch({
-                type: 'GET_USER',
-                payload: data,
-            });
-        }
-    };
-
-    const getUserRepos = async (login) => {
-        setLoading();
-
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10,
-        });
-
-        const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
-
-        const data = await res.json();
-
-        dispatch({
-            type: 'GET_REPOS',
-            payload: data,
-        });
-    };
-
-    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
-
-    const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
     return (
         <GithubContext.Provider
             value={{
-                users: state.users,
-                loading: state.loading,
-                user: state.user,
-                repos: state.repos,
-                searchUsers,
-                clearUsers,
-                getUser,
-                getUserRepos,
+                ...state,
+                dispatch,
             }}>
             {children}
         </GithubContext.Provider>
