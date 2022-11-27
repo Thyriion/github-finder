e93df4ecbeac006a325fe1cc73aad43b3cdb2db2
Thyriion <kevinschueler2000@gmail.com>
2022-11-27 21:08:50 +0100

    URL Params für Repo Get Request

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -19,9 +19,14 @@ export const searchUsers = async (text) => {
 };
 
 export const getUserAndRepos = async (login) => {
+    const repoParams = new URLSearchParams({
+        sort: 'created',
+        per_page: 10,
+    });
+
     const [user, repos] = await Promise.all([
         github.get(`/users/${login}`),
-        github.get(`/users/${login}/repos`),
+        github.get(`/users/${login}/repos?${repoParams}`),
     ]);
 
     return { user: user.data, repos: repos.data };
